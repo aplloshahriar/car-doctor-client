3fdc153cde285aedd574dc4d03f7f1e4468faae6
aplloshahriar <aaronshahriar.as@gmail.com>
2023-05-22 12:12:35 +0600

    signup and create context

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/Signup/SignUp";
+
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,14 @@ const router = createBrowserRouter([
         {
             path:'/',
             element:<Home></Home>,
+        },
+        {
+            path:'login',
+            element: <Login></Login>
+        },
+        {
+            path:'signup',
+            element: <SignUp></SignUp>
         }
       ]
     },
