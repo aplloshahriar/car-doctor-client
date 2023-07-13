50e281b215c4365b004755d0e9c75a04a2fa3e4f
aplloshahriar <aaronshahriar.as@gmail.com>
2023-07-13 16:34:00 +0600

    service search button added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ServiceCard from './ServiceCard';
 
 
@@ -6,22 +6,44 @@ const Services = () => {
     const [services, setServices] = useState([]);
     const [Asc, setAsc] = useState(true);
 
+    const inputRef = useRef(null);
+    const [search, setSearch] = useState('');
+
     useEffect(() => {
-        fetch(`http://localhost:5000/services ? sort=${Asc? 'asc' : 'desc'}`)
+        fetch(`http://localhost:5000/services?search=${search}&sort=${Asc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [Asc])
+    }, [Asc, search])
 
-    
+    const handleSearch = event => {
+        console.log(inputRef.current.value);
+        setSearch(inputRef.current.value);
+    }
+
     return (
-        <div className='mt-4'>
+        <div className='mt-8'>
             <div className='text-center'>
-                <h3 className='text-orange-500 text-2xl font-bold'>Service</h3>
+                <h3 className='text-orange-500 text-2xl font-bold'>Services</h3>
                 <h2 className='text-5xl mb-5'>Our Service Area</h2>
                 <p > <small>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </small></p>
-                <button className='mt-5 btn btn-outline btn-info btn-sm'
-                    onClick={()=>setAsc(!Asc)}
-                >{Asc ? "Price: High to Low" : 'Price: Low To High'}</button>
+
+
+                <div className='flex justify-center mt-4 '>
+                    <div className="form-control ">
+                        <div className="input-group ">
+                            <input type="text" ref={inputRef} placeholder="Find  Services" className="input input-bordered input-group-xs" />
+                            <button onClick={handleSearch} className="btn btn-square">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                            </button>
+                        </div>
+                    </div>
+
+
+
+                    <button className=' ml-2 btn btn-outline btn-info '
+                        onClick={() => setAsc(!Asc)}
+                    >{Asc ? "Price: High to Low" : 'Price: Low To High'}</button>
+                </div>
 
 
             </div>
